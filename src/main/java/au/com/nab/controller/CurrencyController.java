4e1ff3cc1f0c2633cdc6325ee45fc077b513e83e
atuld <atul.duggal@sportsbet.com.au>
2019-04-30 11:27:05 +1000

    fixing cors

--- src/main/java/au/com/nab/controller/CurrencyController.java
+++ src/main/java/au/com/nab/controller/CurrencyController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import au.com.nab.model.ProfitResponseModel;
 import au.com.nab.service.CurrencyService;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class CurrencyController {
     
